e0ed02d3e9139a571aa9070d0910f225254feecb
Anirudha Tambolkar <atambol@ncsu.edu>
2018-04-03 03:48:56 -0400

    handled exception due to lack of connectivity to broker

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,27 +1,32 @@
 import psutil
 import time
+import kafka
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
-
-
-def get_stats():
-    epoch = str(int(time.time()))
-    cpu = str(int(psutil.cpu_percent(interval=1)))
-    mem = str(int(psutil.virtual_memory().percent))
-    return ",".join([epoch, cpu, mem])
 
 
 def main():
     producer_topic = 'cpu'
     bootstrap_servers = '172.25.130.9:9092'
-    producer = KafkaProducer(bootstrap_servers=[bootstrap_servers])
     while True:
-        future = producer.send(producer_topic, get_stats())
-        # Block for 'synchronous' sends
         try:
-            future.get(timeout=10)
-        except KafkaError as err:
-            print err
+            last_epoch = str(int(time.time()))
+            producer = KafkaProducer(bootstrap_servers=[bootstrap_servers])
+            while True:
+                epoch = str(int(time.time()))
+                if last_epoch != epoch:
+                    cpu = str(int(psutil.cpu_percent(interval=1)))
+                    mem = str(int(psutil.virtual_memory().percent))
+                    data = ",".join([epoch, cpu, mem])
+                    last_epoch = epoch
+                    future = producer.send(producer_topic, data)
+                    # Block for 'synchronous' sends
+                    try:
+                        future.get(timeout=10)
+                    except KafkaError as err:
+                        print err
+        except kafka.errors.NoBrokersAvailable:
+            pass
 
 
 if __name__ == "__main__":
